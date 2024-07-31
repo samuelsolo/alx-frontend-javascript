261dc6089674497590b24a3a3e970370facb24f9
samuelsolo <samuelsolomon.des@gmail.com>
2024-07-31 00:45:17 +0000

    0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,17 +1,16 @@
 /**
- * Retrieves students in a given location.
+ * Retrieves ids from a list of students.
  * @param {{
  *   id: Number,
  *   firstName: String,
  *   location: String
  * }[]} students - The list of students.
- * @param {String} city - The location.
  * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  * @returns
  */
-export default function getStudentsByLocation(students, city) {
+export default function getListStudentIds(students) {
   if (students instanceof Array) {
-    return students.filter((student) => student.location === city);
+    return students.map((student) => student.id);
   }
   return [];
 }
